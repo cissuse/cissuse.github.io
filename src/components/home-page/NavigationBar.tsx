ad8cc710e34f8627c5e2cb23187506b33abfe416
mk <moonykun@outlook.com>
2025-07-24 17:16:18 +0800

    ✨ feat: 改变背景，添加文章layout

--- src/components/home-page/NavigationBar.tsx
+++ src/components/home-page/NavigationBar.tsx
@@ -1,9 +1,15 @@
-import styles from './homePage.module.css'
+import styles from "./homePage.module.css";
+import Link from "next/link";
 
 export default function NavigationBar() {
-    return (
-        <div className={styles.navigationBarContainer}>
-            this is navigationBar
-        </div>
-    )
+  return (
+    <div className={styles.navigationBarContainer}>
+      <div style={{marginRight: '10px'}}>
+        <Link href="/">首页</Link>
+      </div>
+      <div>
+        <Link href="/archiving">归档</Link>
+      </div>
+    </div>
+  );
 }
